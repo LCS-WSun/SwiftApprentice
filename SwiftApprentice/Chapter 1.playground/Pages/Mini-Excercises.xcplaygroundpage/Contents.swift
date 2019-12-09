c6885cde7e58d16f6a49714fcb240138f1c36242
Wesley Sun <wsun@lcs.on.ca>
2019-12-09 15:19:53 -0500

    Worked on the mini exercise

--- SwiftApprentice/Chapter 1.playground/Pages/Mini-Excercises.xcplaygroundpage/Contents.swift
+++ SwiftApprentice/Chapter 1.playground/Pages/Mini-Excercises.xcplaygroundpage/Contents.swift
@@ -3,3 +3,32 @@ import Cocoa
 var str = "Hello, playground"
 
 //Chapter 1 Mini-Excercises and challenges 
+
+ 
+Int myAge = 16
+
+var averageAge: Double 16
+
+let testNumber = 14
+
+let evenOdd = testNumber
+
+var answer = (0 + 10) * 10 >> 3 
+
+
+
+//1. Declare a constant of type Int called myAge and set it to your age.
+//2. Declare a variable of type Double called averageAge. Initially, set it to your own
+//age. Then, set it to the average of your age and my own age of 30.
+//3. Create a constant called testNumber and initialize it with whatever integer you’d
+//like. Next, create another constant called evenOdd and set it equal to testNumber
+//modulo 2. Now change testNumber to various numbers. What do you notice
+//about evenOdd?
+//4. Create a variable called answer and initialize it with the value 0. Increment it by
+//1. Add 10 to it. Multiply it by 10.
+
+
+
+
+
+
