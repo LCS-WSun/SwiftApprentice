dcf1210970e9a131d7417e5445573e87718ac71f
Wesley Sun <wsun@lcs.on.ca>
2019-12-12 11:00:42 -0500

    Working through the labs in swift apprentice

--- SwiftApprentice/Chapter 1.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
+++ SwiftApprentice/Chapter 1.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
@@ -15,7 +15,39 @@ let varDogs = 0 + 1
 
 //Challenge 2: Make it compile.
 
-age: Int = 16
+age: var = 16
 print(age)
 age = 30
 print(age)
+
+// switched it from being a constant to a variable
+
+
+//Challenge 3: Compuete the answer
+
+let x: Int = 46
+let y: Int = 10
+
+
+
+// 1
+let answer1: Int = (x * 100) + y
+// (46 * 100) + 10
+// 4610
+
+// 2
+let answer2: Int = (x * 100) + (y * 100)
+// ( 46 * 100) + (10 * 100)
+// 4600 + 1000
+// 5600
+
+// 3
+let answer3: Int = (x * 100) + (y / 10)
+// (46 * 100) + (10/10)
+// 4600 + 1
+// 4601
+
+
+//Challenge 4
+(8 - 4) * 2 + 6 / 3 * 4
+
