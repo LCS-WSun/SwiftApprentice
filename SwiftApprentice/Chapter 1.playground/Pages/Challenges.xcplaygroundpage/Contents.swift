b301c15a58aa5b9d8fe45aaec8a043865c4ce347
Wesley Sun <wsun@lcs.on.ca>
2019-12-21 14:17:39 -0500

    d

--- SwiftApprentice/Chapter 1.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
+++ SwiftApprentice/Chapter 1.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
@@ -7,6 +7,8 @@ var str = "Hello, playground"
 //: [Next](@next)
 
 
+//Variables and constants chapter playground
+
 //Challenge 1: Variables
 
 let myAge = 16
@@ -15,7 +17,7 @@ let varDogs = 0 + 1
 
 //Challenge 2: Make it compile.
 
-age: var = 16
+var age = 16
 print(age)
 age = 30
 print(age)
@@ -48,6 +50,85 @@ let answer3: Int = (x * 100) + (y / 10)
 // 4601
 
 
-//Challenge 4
+//Challenge 4 - the parathenses challenge
 (8 - 4) * 2 + 6 / 3 * 4
 
+
+//Challenge 5: Average rating
+
+
+let rating1: Double = 2.0
+
+let rating2: Double = 3.0
+
+let rating3: Double = 4.0
+
+(rating1 + rating2 + rating3) /3.0 = AverageRating
+
+
+//Challenge 6: Electrical power
+
+The power of an electrical appliance can be calculated by multiplying the voltage by
+the current. Declare a constant named voltage of type Double and assign it a value.
+Then declare a constant called current of type Double and assign it a value. Finally
+calculate the power of the electrical appliance you’ve just created storing it in a
+constant called power of type Double.
+ 
+
+let voltage: Double = 1.0
+
+let Current: Double = 2.0
+
+voltage * Current = Power
+
+ 
+ 
+
+
+let Power: Double = 2.0
+
+
+//Challenge 7 Electrical Resistance (don't know how to solve it)
+
+
+/*The resistance of such an appliance can be then calculated (in a long-winded way) as
+the power divided by the current squared. Calculate the resistance and store it in a
+constant called resistance of type Double
+ 
+ 
+
+
+
+
+
+//Challenge 8 Random integer (don't know how to solve it)
+
+You can create a random integer number by using the function arc4random(). This
+creates a number anywhere between 0 and 4294967295. You can use the modulo
+operator to truncate this random number to whatever range you want. Declare a
+constant randomNumber and assign it a random number generated with
+arc4random(). Then calculate a constant called diceRoll and use the random
+number you just found to create a random number between 1 and 6.
+ 
+ arc4random()
+ 
+ let randomNumber = >>
+ 
+ let diceRoll =
+ 
+ 
+
+ 
+ Challenge 9 (don't know how to solve)
+ A quadratic equation is something of the form a⋅x² + b⋅x + c = 0. The values of x
+which satisfy this can be solved by using the equation x = (-b ± sqrt(b² -
+4⋅a⋅c)) / (2⋅a). Declare three constants named a, b and c of type Double. Then
+calculate the two values for x using the equation above (noting that the ± means plus
+or minus — so one value of x for each). Store the results in constants called root1
+and root2 of type Double.
+ 
+
+ a =
+ 
+ 
+÷
