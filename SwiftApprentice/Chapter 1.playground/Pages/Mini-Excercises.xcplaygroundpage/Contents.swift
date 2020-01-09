bbdb761fe9bd883d99c896588f5bee98dff9ece0
Wesley Sun <wsun@lcs.on.ca>
2020-01-09 14:55:05 -0500

    Working on mini exercise 2 swift apprentice

--- SwiftApprentice/Chapter 1.playground/Pages/Mini-Excercises.xcplaygroundpage/Contents.swift
+++ SwiftApprentice/Chapter 1.playground/Pages/Mini-Excercises.xcplaygroundpage/Contents.swift
@@ -28,8 +28,50 @@ var answer = (0 + 10) * 10 >> 3
 //1. Add 10 to it. Multiply it by 10.
 
 
+/* Mini-exercises Chapter 2 Operators
+1. Create a constant called age1 and set it equal to 42. Create a constant called age2
+and set it equal to 21. Check using Option-click that the type for both has been
+inferred correctly as Int.
+2. Create a constant called avg1 and set it equal to the average of age1 and age2
+using the naïve operation (age1 + age2) / 2. Use Option-click to check the
+type and check the result of avg1. Why is it wrong?
+3. Correct the mistake in the above exercise by converting age1 and age2 to type
+Double in the formula. Use Option-click to check the type and check the result of
+avg1. Why is it now correct?? */
+ 
+let age1: Double = 42
+ 
+let age2: Double = 21
+
+let avg1 = (age1 + age2) / 2
+//it is wrong because avg1 is a type double
 
 
 
 
+
+Mini Excercise Strings Swift Apprentice
+
+
+//1. Create a string constant called firstName and initialize it to your first name.
+//Also create a string constant called lastName and initialize it to your last name.
+
+let firstName = Wesley
+
+let lastName
+
+
+
+
+
+
+
+2. Create a string constant called fullName by adding the firstName and lastName
+constants together, separated by a space.
+3. Using interpolation, create a string constant called myDetails that uses the
+fullName constant to create a string introducing yourself. For example, my string
+would read: "Hello, my name is Matt Galloway.".
+
+
+ 
 
